dd9b994692f397f247150692366aaab1bfc9bb75
Eklavya Dahiya <eklavyadahiya@gmail.com>
2023-04-20 22:59:48 +0200

    Workflow

--- src/Munch.js
+++ src/Munch.js
@@ -1,5 +1,4 @@
 import Navigation from './components/nav/Navigation';
-import logo from './logo.svg';
 import './Munch.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
